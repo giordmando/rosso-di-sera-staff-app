6944c3b6ff5427c0361e93aa1e7a44b4563c1c90
giordmando <81192948+giordmando@users.noreply.github.com>
2026-06-10 23:21:59 +0200

    Add powered by footer component

---
+++ src/components/AppFooter.tsx
@@ -0,0 +1,10 @@
+export function AppFooter() {
+  return (
+    <footer className="app-footer">
+      <div className="container footer-inner">
+        <span>Rosso di Sera Staff</span>
+        <span>Powered by Giordano Mandolesi</span>
+      </div>
+    </footer>
+  );
+}
